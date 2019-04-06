2a7210c8dc5d79c81a1ed65358e001045e1bffc8
GaetanoMondelli <gaetano.mondelli@gmail.com>
2019-04-06 22:39:45 +0100

    docs: remove vew source

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -201,3 +201,5 @@ epub_exclude_files = ['search.html']
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
+
+html_show_sourcelink = False
