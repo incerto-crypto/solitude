5264502133ea73c997d8d0997017cba86509a887
GaetanoMondelli <gaetano.mondelli@gmail.com>
2019-04-12 20:59:52 +0100

    ci: added all parameter

--- ci/script.sh
+++ ci/script.sh
@@ -2,13 +2,14 @@
 if [ "$TRAVIS_BRANCH" == "feature/ci-cd" ]; then
     #TO-DO: change feature/ci-cd -> master 
     pip install -r ci/requirements-ci.txt
-    git fetch
+    git fetch --all
     git branch -a
     cd docs
     make html
     git add build -f
     git commit -m $TRAVIS_COMMIT
     git branch temp-branch
+    git branch -a
     git checkout gh-pages
     git merge temp-branch
     git push origin gh-pages
